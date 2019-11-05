3193a571ef2506edcea0556e213b25738357a2d6
Manju Gowda <56513486+gowdam@users.noreply.github.com>
2019-11-05 20:24:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function disp(num) {
+    document.getElementById("input").value += num;
+}
+
+function allClr() {
+    document.getElementById("input").value = "";
+    document.getElementById("output").value = "0";
+
+}
+
+function clr() {
+    var clear = document.getElementById("input").value;
+    var clearLastNum = clear.substr(0, (clear.length - 1));
+    document.getElementById("input").value = clearLastNum;
+}
+
+function solve() {
+    let x = document.getElementById("input").value;
+    let y = eval(x);
+    document.getElementById("output").value = y;
+}
